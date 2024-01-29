3e7d17a1bbc75c04853d947d0de670cc59135fbe
Kieran Wake <kieranwake1234@gmail.com>
2024-01-29 12:39:59 +0000

    implemented the getDate() method in attempt to extrapolate the day from the provided date parameter

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 function timeForMilkAndCookies(date) {
-    const d = new Date("December 24th"); 
+    
 
-    if (d === new Date) {
-        return true;
-    } else {
-        return false;
-    }
 }
 
-console.log(timeForMilkAndCookies(date)); 
