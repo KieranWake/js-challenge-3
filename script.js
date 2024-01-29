c2891afb75f43c29bd4f9323c1780ed580f025ce
Kieran Wake <kieranwake1234@gmail.com>
2024-01-29 11:40:01 +0000

    declared const variable equal to new date, which takes a parameter in a form of string representing the date, proceeding to integrate if statement for conditions

---
+++ script.js
@@ -0,0 +1,11 @@
+function timeForMilkAndCookies(date) {
+    const d = new Date("December 24th"); 
+
+    if (d === new Date) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(timeForMilkAndCookies(date)); 
